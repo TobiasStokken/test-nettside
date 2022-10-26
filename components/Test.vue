7858a35157df64fe85b8b7e50b2aa8e3d62ce2ea
ChipsGodt <chipsgodt@gmail.com>
2022-10-26 14:15:16 +0200

    initial commit

---
+++ components/Test.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="test">
+    <div class="test3">Hei</div>
+    <div>
+      <input v-model="tekst" class="test2" type="text">
+    </div>
+  <div class="test4"><h1>{{ tekst.split('').reverse().join('') }}</h1></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tekst: ''
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+.test {
+  left: 50%;
+  top: 50;
+    display: inline-block;
+  transform: translate(-50,-50);
+
+}
+.test2:hover{
+    transform: scale(105%);
+    transition: transform 0.5s;
+    
+}
+.test2 {
+  font-size: 50%;
+  color: black;
+  border-radius: 50vh;
+}
+.test3{
+    display: flex;
+    justify-content: center;
+}
+.test4{
+    display: flex;
+    justify-content: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <Test/>
+</template>
+
+<style>
+body {
+  background: #00223d;
+  color: white;
+  font-size: 500%;
+  display: flex;
+	min-height: 100vh;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
