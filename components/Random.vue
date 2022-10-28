9616fef224f030d5ddc3bd72b5aedea82b6f420c
ChipsGodt <chipsgodt@gmail.com>
2022-10-28 08:20:36 +0200

    update

---
+++ components/Random.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>Tekst: {{text}}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: params.get('text')
+    }
+  }
+}
+
+let url = "https://uselessfacts.jsph.pl/random.json?language=en";
+
+let params = (new URL(url)).searchParams;
+let text = params.get('text');
+console.log(text)
+
+</script>
---
+++ pages/random.vue
@@ -0,0 +1,17 @@
+<template>
+    <Random/>
+
+</template>
+
+<style>
+body {
+  background: #00223d;
+  color: white;
+  font-size: 500%;
+  display: flex;
+	min-height: 100vh;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+</style>
