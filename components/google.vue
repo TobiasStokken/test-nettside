92c946a70e194598b2e8b61b45c7485d273e5696
Tobias Stokken <tobias@stokken.net>
2022-11-11 12:59:20 +0100

    update

---
+++ components/google.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="alt">
+    <div>Her kan du google ting</div>
+    <div class="inputdiv">
+      <input class="input" v-model="tekst" type="text" />
+    </div>
+    <div class="googleButton">
+      <button v-on:click="google()">Google</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tekst: "e",
+    };
+  },
+  methods: {
+    google() {
+      console.log("https://google.com/" + this.tekst);
+      window.location.replace("https://google.com/search/" + this.tekst);
+    },
+  },
+};
+</script>
+
+<style>
+.alt {
+  transform: translate(-50, -50);
+  background: #00223d;
+  font-size: 500%;
+  display: flex;
+  color: white;
+  overflow: hidden;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.inputdiv {
+  transition: transform 0.5s;
+  font-size: 50%;
+  color: black;
+}
+.input {
+  border-radius: 2vh;
+}
+</style>
